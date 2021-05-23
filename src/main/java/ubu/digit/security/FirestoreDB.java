d197b3bc8be63336d535b838951ac6bba8ac1650
diana.bringas <dbo1001@alu.ubu.es>
2021-05-23 13:59:53 +0200

    Creación clase FirestoreDB #122

---
+++ src/main/java/ubu/digit/security/FirestoreDB.java
@@ -0,0 +1,128 @@
+package ubu.digit.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.api.core.ApiFuture;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
+
+import java.io.IOException;
+
+/**
+ * Clase de conexión y administración de Firestore 
+ * 
+ * @author Diana Bringas Ochoa
+ */
+public class FirestoreDB {
+
+	/**
+	 * Logger de la clase.
+	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(FirestoreDB.class.getName());
+	
+	/**
+	 * Conexión con la base de datos de Firestore 
+	 */
+	private Firestore connectionDB = null;
+	
+	/**
+	 * Instancia de la clase FirestoreDB
+	 */
+	private static FirestoreDB instance = null;
+	
+	/**
+	 * Usuario loggeado
+	 */
+	private static String user = "";
+    
+    /**
+	 * Constructor que inicia la conexión con la Firestore.
+	 */
+	private FirestoreDB() {
+		this.connectionDB = this.getConectionFirectore();
+	}
+
+	/**
+	 * Método singleton para obtener la instancia de FirestoreDB
+	 */
+	public static FirestoreDB getInstance(){
+		if (instance == null) {
+			instance = new FirestoreDB();
+		}
+		return instance;
+	}
+	
+    /**
+	 * Comprueba si el usuario y la contraseña introducidos son correctos.
+	 * @param email
+	 * @param password
+	 * @return bool
+	 */
+	public Boolean authenticate(String email, String password){
+		try {
+			DocumentReference docRef = connectionDB.collection("Users").document("Data_Users");
+			ApiFuture<DocumentSnapshot> users = docRef.get();
+
+			DocumentSnapshot document = users.get();
+			if (document.exists()) {
+				if(document.getData().get(email).toString().equals(password)) {
+					user = email;
+					return true;
+				}
+				LOGGER.info("PASSWORD: " + document.getData().get(email));
+			} else {
+				LOGGER.error("No existe ese documento en la base de datos");
+			}
+		}catch(Exception e) {
+			LOGGER.error("Error al extraer la información de la base de datos : ", e);
+		}
+		return false;
+	}
+	
+	/**
+	 * Verifica si se ha iniciado sesión
+	 * @return bool
+	 */
+	public static boolean isUserLogin() {
+		if(user != "") {
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Cerrar sesión del usuario actual
+	 */
+	public static void logout() {
+		if(user != "") {
+			user = "";
+		}
+	}
+	
+	/**
+	 * Se establece la conexión con la base de datos de Firebase, Firestore.
+	 * A través de la cual se obtendrán los datos de los usuarios.
+	 * 
+	 * @return base de datos Firestore
+	 */
+	private Firestore getConectionFirectore() {
+		try {
+			FirestoreOptions firestoreOptions =
+				    FirestoreOptions.getDefaultInstance().toBuilder()
+				        .setProjectId("gestor-tfg-2021")
+				        .setCredentials(GoogleCredentials.getApplicationDefault())
+				        .build();
+			
+				return firestoreOptions.getService();
+				
+		}catch(IOException e) {
+			LOGGER.error("Opciones de configuración de Firestore invalidas : ", e);
+		}
+		return null;
+	}
+}
+
--- src/main/java/ubu/digit/ui/views/LoginView.java
+++ src/main/java/ubu/digit/ui/views/LoginView.java
@@ -1,46 +1,30 @@
 package ubu.digit.ui.views;
 
-import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.google.api.core.ApiFuture;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.DocumentSnapshot;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.FirestoreOptions;
-import com.google.common.io.BaseEncoding;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.*;
-import com.google.firebase.cloud.FirestoreClient;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.internal.FirebaseService;
 import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.login.LoginI18n;
-import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.PasswordField;
-import com.vaadin.flow.component.textfield.TextField;
 
+import ubu.digit.security.FirestoreDB;
 import ubu.digit.ui.MainLayout;
-import ubu.digit.util.ExternalProperties;
+import ubu.digit.ui.components.Footer;
+
 /**
  * Vista de inicio de sesión.
  * 
- * @author Javier de la Fuente Barrios
+ * @author Diana Bringas Ochoa
  */
-
 @Route(value = "Login", layout = MainLayout.class)
 @PageTitle("Login ")
 public class LoginView extends VerticalLayout{
+
+	private static final long serialVersionUID = 1L;
 
 	/**
 	 * Logger de la clase.
@@ -58,25 +42,14 @@ public class LoginView extends VerticalLayout{
 	private LoginForm login;
 	
 	/**
-	 * Punto de entrada del SDKs de Firebase
+	 * Instancia de la Base de datos de Firestore 
 	 */
-	private FirebaseApp firebaseApp = null;
-	
-	/**
-	 * Base de datos de Firestore 
-	 */
-	private Firestore db = null;
-	
-	/**
-	 * Usuario loggeado
-	 */
-	private String user = "";
+	private static FirestoreDB firestore = null;
 	
 	/**
 	 * Constructor donde se crea el login
 	 */
 	public LoginView() {
-		getInstanceDatabase();
 
 		addClassName("login-view");
 		setMargin(true);
@@ -86,22 +59,24 @@ public class LoginView extends VerticalLayout{
 		setJustifyContentMode(JustifyContentMode.CENTER);
 		setAlignItems(Alignment.CENTER);
 
+		firestore = FirestoreDB.getInstance();
+		
 		login = new LoginForm();
-		login.setAction("Login");
 		login.setI18n(createSpanishI18n());
 		login.setForgotPasswordButtonVisible(false);
 		login.addLoginListener(e -> {
 			login.setEnabled(false);
-		    boolean isAuthenticated = authenticate(e.getUsername(), e.getPassword());
+		    boolean isAuthenticated = firestore.authenticate(e.getUsername(), e.getPassword());
 		    if (isAuthenticated) {
 		    	UI.getCurrent().navigate(UploadView.class);
 		    } else {
 		    	login.setEnabled(true);
-		    	 login.setError(true);
+		    	login.setError(true);
 		    }
 		});
-			
 		add(login);
+		Footer footer = new Footer(null);
+		add(footer);
 	}
 	
 	/**
@@ -124,78 +99,5 @@ public class LoginView extends VerticalLayout{
 	    i18n.getErrorMessage()
 	        .setMessage("Verifique su usuario y contraseña y vuelva a intentarlo.");
 	    return i18n;
-	}
-	        
-	/**
-	 * Comprueba si el usuario y la contraseña introducidos son correctos.
-	 * @param email
-	 * @param password
-	 * @return bool
-	 */
-	private Boolean authenticate(String email, String password){
-		try {
-			DocumentReference docRef = db.collection("Users").document("Data_Users");
-			ApiFuture<DocumentSnapshot> users = docRef.get();
-
-			DocumentSnapshot document = users.get();
-			if (document.exists()) {
-				if(document.getData().get(email).toString().equals(password)) {
-					user = email;
-					return true;
-				}
-				LOGGER.info("PASSWORD: " + document.getData().get(email));
-			} else {
-				LOGGER.error("No existe ese documento en la base de datos");
-			}
-		}catch(Exception e) {
-			LOGGER.error("Error al extraer la información de la base de datos : ", e);
-		}
-		return false;
-	}
-	
-	/**
-	 * Verifica si se ha iniciado sesión
-	 * @return bool
-	 */
-	public boolean isUserLogin() {
-		if(user != "") {
-			return true;
-		}
-		return false;
-	}
-	
-	public boolean logout() {
-		return false;
-	}
-	
-	/**
-	 * Método singleton para obtener la conexión con la base de datos de Firebase
-	 */
-	private void getInstanceDatabase() {
-		if (db == null) {
-			db = getConectionFirectore();
-		}
-	}
-	
-	/**
-	 * Se establece la conexión con la base de datos de Firebase, Firestore.
-	 * A través de la cual se obtendrán los datos de los usuarios.
-	 * 
-	 * @return base de datos Firestore
-	 */
-	private Firestore getConectionFirectore() {
-		try {
-			FirestoreOptions firestoreOptions =
-				    FirestoreOptions.getDefaultInstance().toBuilder()
-				        .setProjectId("gestor-tfg-2021")
-				        .setCredentials(GoogleCredentials.getApplicationDefault())
-				        .build();
-			
-				return firestoreOptions.getService();
-				
-		}catch(IOException e) {
-			LOGGER.error("Opciones de configuración de Firestore invalidas : ", e);
-		}
-		return null;
 	}
 }
