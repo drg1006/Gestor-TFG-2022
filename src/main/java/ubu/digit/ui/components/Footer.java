41477c7c863754193b33c37413b8fd2c4ad5ff63
diana.bringas <dbo1001@alu.ubu.es>
2021-05-23 14:23:51 +0200

    Actualizar Footer - iconos y fechas actualización de ficheros #125
    Se han incorporado los iconos faltantes y modificado la obtención de la fecha de actualización de los ficheros de data. Además se han incorporado los participantes del proyecto faltantes.

--- src/main/java/ubu/digit/ui/components/Footer.java
+++ src/main/java/ubu/digit/ui/components/Footer.java
@@ -4,12 +4,13 @@ import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Anchor;
-import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import  com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
-import ubu.digit.pesistence.SistInfDataXls;
 import ubu.digit.ui.views.LoginView;
 import ubu.digit.util.ExternalProperties;
 
@@ -22,24 +23,34 @@ import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
  * Pié de página común a todas las vistas.
  * 
  * @author Javier de la Fuente Barrios
- *
+ * @author Diana Bringas Ochoa
  */
-public class Footer extends HorizontalLayout {
+public class Footer extends VerticalLayout {
 
 	/**
 	 * Serial Version UID.
 	 */
 	private static final long serialVersionUID = 1285443082746553886L;
 	
+	/**
+	 * Layout que contiene los layout verticales (columnas)
+	 * de information y license
+	 */
 	HorizontalLayout conten;
 	
+	/**
+	 * Layout correspondiente a la columna con la licencia, 
+	 * fecha de actualización de los ficheros y acceso al login.
+	 */
 	VerticalLayout license;
 	
+	/**
+	 * Layout con la información sobre los autores y tutores de la app
+	 */
 	VerticalLayout information;
 	
 	/**
@@ -52,53 +63,59 @@ public class Footer extends HorizontalLayout {
 	 */
 	private static final Logger LOGGER = LoggerFactory.getLogger(Footer.class.getName());
 
-
 	/**
 	 * Constructor.
-	 * 
 	 */
 	public Footer(String fileName) {
 		this.fileName = fileName;
 		conten = new HorizontalLayout();
 		conten.addClassName("Footer-grid");
 		
+		H2 subtitle = new H2(INFORMACION);
+		subtitle.addClassName(SUBTITLE_STYLE);
+		
 		addInformation();
 		addLicense();
 		conten.add(information, license);
 		
-		add(conten);
-		
+		add(subtitle, conten);
 	}
 
 	/**
-	 * Añade la información del proyecto.
+	 * Añade la información de los autores y tutores del proyecto.
 	 */
 	private void addInformation() {
 		information = new VerticalLayout();
 		information.setMargin(false);
 		information.setSpacing(true);
-		
-		Label subtitle = new Label(INFORMACION);
-		subtitle.addClassName(SUBTITLE_STYLE);
-		information.add(subtitle);
+
+		Text version1 = new Text("Versión 1.0 creada por Beatriz Zurera Martínez-Acitores");
+		Anchor link1 = new Anchor("mailto:bzm0001@alu.ubu.es","bzm0001@alu.ubu.es");
+		information.add(new HorizontalLayout(new Icon(VaadinIcon.ENVELOPE),link1));
+		information.add(version1);
 		
 		Text version2 = new Text("Versión 2.0 creada por Javier de la Fuente Barrios");
 		Anchor link2 = new Anchor("mailto:jfb0019@alu.ubu.es","jfb0019@alu.ubu.es");
+		information.add(new HorizontalLayout(new Icon(VaadinIcon.ENVELOPE),link2));
 		information.add(version2);
-		information.add(link2);
 		
-		Text version1 = new Text("Versión 1.0 creada por Beatriz Zurera Martínez-Acitores");
-		Anchor link1 = new Anchor("bzm0001@alu.ubu.es","mailto:bzm0001@alu.ubu.es");
-		information.add(version1);
-		information.add(link1);
+		Text version3 = new Text("Versión 3.0 creada por Diana Bringas Ochoa");
+		Anchor link3 = new Anchor("mailto:dbo1001@alu.ubu.es","dbo1001@alu.ubu.es");
+		information.add(new HorizontalLayout(new Icon(VaadinIcon.ENVELOPE),link3));
+		information.add(version3);
 		
-		Text tutor = new Text("Tutorizado por Carlos López Nozal");
-		Anchor linkT = new Anchor("clopezno@ubu.es", "mailto:clopezno@alu.ubu.es");
-		information.add(tutor);
-		information.add(linkT);
+		Text tutor1 = new Text("Tutorizado por Carlos López Nozal");
+		Anchor linkT1 = new Anchor("mailto:clopezno@alu.ubu.es","clopezno@ubu.es");
+		information.add(new HorizontalLayout(new Icon(VaadinIcon.ENVELOPE),linkT1));
+		information.add(tutor1);
+		
+		Text tutor2 = new Text("Tutorizado por Alvar Arnaiz Gonzalez");
+		Anchor linkT2 = new Anchor("mailto:alvarag@alu.ubu.es", "alvarag@ubu.es");
+		information.add(new HorizontalLayout(new Icon(VaadinIcon.ENVELOPE),linkT2));
+		information.add(tutor2);
 		
 		Text copyright = new Text("Copyright @ LSI");
-		information.add(copyright);
+		information.add(new HorizontalLayout(copyright));
 	}
 	
 	/**
@@ -119,7 +136,7 @@ public class Footer extends HorizontalLayout {
 		
 		LOGGER.info("Ruta config.properties Footer " + dir);
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		String lastModified = null;
 		File file = new File(dir + fileName);
 		if (file.exists()) {
@@ -130,25 +147,24 @@ public class Footer extends HorizontalLayout {
 	}
 
 	/**
-	 * Añade la información de la licencia del proyecto.
+	 * Añade la información de la licencia del proyecto, la fecha de actualización 
+	 * de los ficheros de datos y el butón del acceso al Login
 	 */
 	private void addLicense() {
 		license = new VerticalLayout();
 		license.setMargin(false);
 		license.setSpacing(true);
 
-		Image ccImage= new Image("./styles/img/cc.png","https://creativecommons.org/licenses/by/4.0/");
-	
-		Text licenseText = new Text("This work is licensed under a: ");
-		Anchor ccLink = new Anchor("https://creativecommons.org/licenses/by/4.0/","Creative Commons Attribution 4.0 International License.");
-
-		license.add(ccImage);
-		license.add(licenseText);
-		license.add(ccLink);
+		Text licenseText = new Text("This work is licensed under: ");
+		Anchor ccLink = new Anchor("https://creativecommons.org/licenses/by/4.0/", "Creative Commons Attribution 4.0 International License.");
+		
+		license.add(licenseText, ccLink);
 		
 		if (fileName != null) {
-			String lastModified = getLastModified(fileName);
-			license.add(new Label("Ultima actualización: " + lastModified));
+			String lastModifiedCsv = getLastModified(fileName);
+			String lastModifiedXls = getLastModified("BaseDeDatosTFGTFM.xls");
+			license.add(new Label("Ultima actualización de " + fileName + " : " + lastModifiedCsv));
+			license.add(new Label("Ultima actualización de BaseDeDatosTFGTFM.xls : " + lastModifiedXls));
 		}
 
 		Button login = new Button("Actualizar");
