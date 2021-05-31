632bc7793a78f46578cd260e91222503b7b909df
diana.bringas <dbo1001@alu.ubu.es>
2021-05-31 20:33:27 +0200

    Actualizar Métricas con el cambio de vista a InformationView #127

--- src/main/java/ubu/digit/ui/views/MetricsView.java
+++ src/main/java/ubu/digit/ui/views/MetricsView.java
@@ -1,12 +1,18 @@
 package ubu.digit.ui.views;
 
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.IFrame;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.server.ExternalResource;
+import com.vaadin.server.ThemeResource;
+import com.vaadin.ui.Embedded;
+import com.vaadin.ui.Link;
 
-import ubu.digit.ui.MainLayout;
 import ubu.digit.ui.components.Footer;
+import ubu.digit.ui.components.NavigationBar;
 import ubu.digit.util.ExternalProperties;
 import static ubu.digit.util.Constants.*;
 
@@ -14,8 +20,10 @@ import static ubu.digit.util.Constants.*;
  * Vista de métricas.
  * 
  * @author Javier de la Fuente Barrios
+ * @author Diana Bringas Ochoa
+ * 
  */
-@Route(value = "Metrics", layout = MainLayout.class)
+@Route(value = "Metrics")
 @PageTitle("Métricas")
 public class MetricsView extends VerticalLayout{
 
@@ -42,11 +50,33 @@ public class MetricsView extends VerticalLayout{
 		setMargin(true);
 		setSpacing(true);
 		
+		NavigationBar bat = new NavigationBar();
+		add(bat);
+		
 		H1 metricsTitle = new H1(METRICAS);
 		metricsTitle.addClassName(TITLE_STYLE);
 		add(metricsTitle);
 		
+		addSonarCloudLink();
+		
 		Footer footer = new Footer(null);
 		add(footer);
 	}
+	
+	/**
+	 * Añadir análisis de la calidad del código de los proyectos realizados 
+	 * con SonarCloud.
+	 */
+	private void addSonarCloudLink(){
+		String urlSonar = config.getSetting("urlSonar");
+
+		IFrame sonarCloud = new IFrame("https://sonarcloud.io/organizations/dbo1001/projects");
+		sonarCloud.setWidth(85, Unit.PERCENTAGE);
+		sonarCloud.setHeight(500, Unit.PIXELS);
+		//sonarCloud.setAllow("encrypted-media; gyroscope; picture-in-picture");
+		sonarCloud.getElement().setAttribute("frameborder", "0");
+		sonarCloud.getElement().setAttribute("X-Frame-Options", "*");
+		add(sonarCloud);
+	}
+
 }
