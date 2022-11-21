963ee89fbb0f41dcd19803cb414fbd2af5590387
David Renedo Gil <drg1006@alu.ubu.es>
2022-11-21 12:49:40 +0100

    Sacar el departamento de cada profesor
    #40

--- Documentacion/Pruebas/PruebaWebScrap/src/main/java/Prueba/PruebaWebScrap2.java
+++ Documentacion/Pruebas/PruebaWebScrap/src/main/java/Prueba/PruebaWebScrap2.java
@@ -26,6 +26,7 @@ public class PruebaWebScrap2{
     	
     	Response response = null;
 		try {
+			//Realizamos la petición de la url
 			response = Jsoup.connect("https://investigacion.ubu.es/unidades/2682/investigadores")
 			       .ignoreContentType(true)
 			       .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")  
@@ -48,16 +49,54 @@ public class PruebaWebScrap2{
 			
             // Busco todas las entradas que estan dentro de: 
             Elements entradas = doc.select("div.c-persona-card__detalles");
-            System.out.println("Número de entradas en la página inicial : "+entradas.size()+"\n");
+            System.out.println("Número de profesores de la EPS : "+entradas.size()+"\n");
             // Paseo cada una de las entradas
             for (Element elem : entradas) {
-
+            	
+            	/*Referencia de estas dos lineas de código:
+            	 * https://stackoverflow.com/questions/30408174/jsoup-how-to-get-href*/
+            	
+            	//Cogemos la url de detalles para posteriormente coger su departamento
+            	Element link = elem.select("div.c-persona-card__detalles > a").first();
+            	String url = link.absUrl("href");
+            	
+            	//Cogemos los elementos que necesitamos
                 String nombre = elem.getElementsByClass("c-persona-card__nombre").text();
                 String apellidos = elem.getElementsByClass("c-persona-card__apellidos").text();
                 String area = elem.getElementsByClass("c-persona-card__area").text();
-				
-                System.out.println(nombre+"\n"+apellidos+"\n"+area+"\n\n");
-				
+				//Imprimimos por pantalla
+                System.out.println("Nombre: "+nombre);
+                System.out.println("Apellidos: " +apellidos);
+                System.out.println("Area: "+area);
+
+                //Para sacar el Departamento debemos ir a otra url 
+                try {
+        			response = Jsoup.connect(url)
+        			       .ignoreContentType(true)
+        			       .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")  
+        			       .referrer("http://www.google.com")   
+        			       .timeout(12000) 
+        			       .followRedirects(true)
+        			       .execute();
+        		} catch (IOException e) {
+        			// TODO Auto-generated catch block
+        			e.printStackTrace();
+        		}
+
+            	Document doc2 = null;
+        		try {
+        			doc2 = response.parse();
+        		} catch (IOException e) {
+        			// TODO Auto-generated catch block
+        			e.printStackTrace();
+        		}
+        		//Obtenemos el contenido donde está la información del profesor
+        		Elements entrada= doc2.select("div.main-content");
+        		//Cogemos el primer link de tipo "a"(href) que tiene la información sobre el departamento y sacamos su texto
+        		Element link2= entrada.select("a").first();
+        		String departamento = link2.text();	
+        		System.out.println("Departamento: "+ departamento + "\n");
+
                 // Con el método "text()" obtengo el contenido que hay dentro de las etiquetas HTML
                 // Con el método "toString()" obtengo todo el HTML con etiquetas incluidas
             }
