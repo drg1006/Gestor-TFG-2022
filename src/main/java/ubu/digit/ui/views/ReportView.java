293fabd453bf77e67b5daa07c16a690924de77e4
David Renedo Gil <drg1006@alu.ubu.es>
2022-11-28 20:42:46 +0100

    Update ReportView.java
    #44

--- src/main/java/ubu/digit/ui/views/ReportView.java
+++ src/main/java/ubu/digit/ui/views/ReportView.java
@@ -1,28 +1,33 @@
 package ubu.digit.ui.views;
 
-import static ubu.digit.util.Constants.INFO_ESTADISTICA;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.sql.SQLException;
 import java.text.NumberFormat;
 
 import java.time.format.DateTimeFormatter;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.checkbox.CheckboxGroup;
 import com.vaadin.flow.component.checkbox.CheckboxGroupVariant;
-import com.vaadin.flow.component.combobox.ComboBox;
-import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
-import com.vaadin.flow.component.textfield.IntegerField;
+
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
 
 import ubu.digit.persistence.SistInfDataAbstract;
 import ubu.digit.persistence.SistInfDataFactory;
@@ -114,7 +119,7 @@ public class ReportView extends VerticalLayout {
 	    checkboxGroup.setItems(areas);
 	    
 	    checkboxGroup.addThemeVariants(CheckboxGroupVariant.MATERIAL_VERTICAL);
-	    /*checkboxGroup.addValueChangeListener(event -> {
+	    checkboxGroup.addValueChangeListener(event -> {
 	        if (event.getValue().size() == areas.size()) {
 	            checkbox.setValue(true);
 	            checkbox.setIndeterminate(false);
@@ -124,33 +129,67 @@ public class ReportView extends VerticalLayout {
 	        } else {
 	            checkbox.setIndeterminate(true);
 	        }
-	    });*/
+	    });
 	    checkbox.addValueChangeListener(event -> {
 	        if (checkbox.getValue()) {
 	            checkboxGroup.setValue(new HashSet<>(areas));
 	        } else {
 	            checkboxGroup.deselectAll();
 	        }
+
 	    });
-	    //checkboxGroup.select(areas.get(0), areas.get(2));
+	    //SELECCIONAR POR DEFECTO
+	        //checkboxGroup.select(areas.get(0), areas.get(2));
+	    //ARRAY CON LA LISTA DE AREAS SELECCIONADAS
+            //System.out.println("Value changed:"+ checkboxGroup.getValue());
 	    
+	    //TEXTO PARA AÑADIR NOMBRE AL INFORME
 	    TextField nombreInforme = new TextField();
 	    nombreInforme.setLabel("Indique el nombre del informe");
 	    nombreInforme.setWidth("25%");
-	    nombreInforme.setValue("Informe.xls");
-	    Label informe=new Label("Se creará un archivo con el nombre"+ nombreInforme.getValue() + ".xls");
+
 	    nombreInforme.addValueChangeListener(event ->{
-	        
 	       nombreInforme.setValue(event.getValue());
-	       Label nombre=new Label("Se creará un archivo con el nombre"+ nombreInforme.getValue() + ".xls");
-	       
-	       replace(informe,nombre);
+	       System.out.println(nombreInforme.getValue());
 	    });
 	    
+	    //BOTON PARA CREAR EL INFORME 
+	    Button crearInforme = new Button("Crear Informe");
+	    crearInforme.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+	    add(checkbox, checkboxGroup,nombreInforme,crearInforme);
 	    
-	    add(checkbox, checkboxGroup,nombreInforme,informe);
-	    
+	    crearInforme.addClickListener(event -> {
+	        creacionInforme(checkboxGroup.getValue(),nombreInforme.getValue());
+	        
+	    });
+
 	}
-	   
+	
+	   public void creacionInforme(Set<String> listaAreas, String nombreInforme) {
+	       
+	       File archivo = new File(nombreInforme+".xls");
+	       //String absPath=archivo.getAbsolutePath();
+	       Workbook workbook = new HSSFWorkbook();
+	       
+	        
+	       //int tamano =listaAreas.size();
+	      
+	       
+	       try {
+	            //FileInputStream inputStream = new FileInputStream(new File(absPath));
+	            //Workbook workbook = WorkbookFactory.create(inputStream);
+	            for(String area: listaAreas) {
+	                workbook.createSheet(area);
+	            }
+	            //Se genera el documento
+	            FileOutputStream out = new FileOutputStream(archivo);
+	            workbook.write(out);
+	            workbook.close();
+	            out.close();
+	            Notification notification = Notification.show("Se ha generado el fichero en " + archivo);
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	   }
 	   
 }
