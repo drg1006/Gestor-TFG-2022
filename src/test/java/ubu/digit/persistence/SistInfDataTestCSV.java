bef96842af10528b81055d8891dfca3d84d73ac2
David Renedo Gil <drg1006@alu.ubu.es>
2023-01-22 21:20:51 +0100

    Modificación nombre del paquete

--- src/test/java/ubu/digit/persistence/SistInfDataTestCSV.java
+++ src/test/java/ubu/digit/persistence/SistInfDataTestCSV.java
@@ -1,11 +1,13 @@
-package ubu.digit.pesistence;
+package ubu.digit.persistence;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 
+import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -20,31 +22,31 @@ import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import com.codoid.products.exception.FilloException;
-
-import ubu.digit.persistence.SistInfDataXls;
 import ubu.digit.util.ExternalProperties;
+import ubu.digit.persistence.SistInfDataCsv;
 
 /**
- * Conjunto de método que verifican la cobertura de la clase SistInfDataXls.
+ * Conjunto de método que verifican la cobertura de la clase SistInfDataCsv.
  * 
+ * @author Beatriz Zurera Martínez-Acitores
  * @author Diana Bringas Ochoa
  *
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ ExternalProperties.class, SistInfDataXls.class })
-public class SistInfDataTestXLS {
+@PrepareForTest({ ExternalProperties.class, SistInfDataCsv.class })
+public class SistInfDataTestCSV {
 
     /**
      * Clase fachada a testear.
      */
-	SistInfDataXls sistInfData;
+    SistInfDataCsv sistInfData;
 
     /**
      * URL del fichero donde se encuentra el fichero de configuración del test.
      */
-    ExternalProperties test = ExternalProperties.getInstance("/testConfig.properties", true);
-   
+    ExternalProperties test = ExternalProperties
+    		.getInstance("/testConfig.properties", true);
+
     /**
      * Método que se ejecuta antes de cualquier test. Modifica el fichero de
      * configuración, por el propio del test.
@@ -52,25 +54,23 @@ public class SistInfDataTestXLS {
     @Before
     public void setUp() {
         mockStatic(ExternalProperties.class);
-
         when(ExternalProperties.getInstance("/config.properties", false)).thenReturn(test);
-
-        sistInfData = SistInfDataXls.getInstance(); 
+        sistInfData = SistInfDataCsv.getInstance(); 
     }
 
     /**
      * Test que comprueba que el método que obtiene la media de una lista de
      * números funciona correctamente.
-     *
+     * 
      * En este caso, la columna "notas" es una lista de notas desde el 1 hasta
      * el 10 por lo que el resultado que esperamos es 5.5.
      * 
-     * @throws FilloException
+     * @throws SQLException
      */
     @Test
-    public void testAvg(){
-        Number esperado = sistInfData.getAvgColumn("Nota", "N3_Historico");
-        assertEquals(esperado, is((Number) 7.0625F));
+    public void testAvg() throws SQLException {
+        Number esperado = sistInfData.getAvgColumn("Nota", "Prueba");
+        assertEquals(esperado, is((Number) 5.5F));
     }
 
     /**
@@ -78,14 +78,17 @@ public class SistInfDataTestXLS {
      * listado funciona correctamente. En este caso, la columna "numero" tiene
      * como máximo un 9 y la columna "nota" tiene como máximo un 10.
      * 
-     * @throws FilloException
+     * @throws SQLException
      */
     @Test
-    public void testMaxColumn() throws FilloException {
+    public void testMaxColumn() throws SQLException {
 
-        String tableName = "N3_Historico";
+        String tableName = "Prueba";
+        Number num = sistInfData.getMaxColumn("Numero", tableName);
         Number not = sistInfData.getMaxColumn("Nota", tableName);
-        assertEquals(not, is((Number) 9F));
+        assertEquals(num, is((Number) 9.0F));
+        assertEquals(not, is((Number) 10.0F));
+        assertNotEquals(num, not);
     }
 
     /**
@@ -93,13 +96,16 @@ public class SistInfDataTestXLS {
      * listado funciona correctamente. En este caso, la columna "numero" tiene
      * como mínimo un 0 y la columna "nota" tiene como mínimo un 1.
      * 
-     * @throws FilloException
+     * @throws SQLException
      */
     @Test
-    public void testMinColumn() throws FilloException {
-        String tableName = "N3_Historico";
+    public void testMinColumn() throws SQLException {
+        String tableName = "Prueba";
+        Number num = sistInfData.getMinColumn("Numero", tableName);
         Number not = sistInfData.getMinColumn("Nota", tableName);
-        assertEquals(not, is((Number) 4F));
+        assertEquals(num, is((Number) 0.0F));
+        assertEquals(not, is((Number) 1.0F));
+        assertNotEquals(num, not);
     }
 
     /**
@@ -108,32 +114,33 @@ public class SistInfDataTestXLS {
      * un listado de números de 0 a 9 y la columna "nota" un listado de 1 a 10,
      * por lo que en ambas esperamos la misma desviación.
      * 
-     * @throws FilloException
+     * @throws SQLException
      */
     @Test
-    public void testStdvColumn() throws FilloException { 
-        String tableName = "N3_Historico";
+    public void testStdvColumn() throws SQLException { 
+        String tableName = "Prueba";
+        Number num = sistInfData.getStdvColumn("Numero", tableName);
         Number not = sistInfData.getStdvColumn("Nota", tableName);
-        assertEquals(not, is((Number) 1.8980723303996008));
+        assertEquals(num, is((Number) 3.0276503540974917));
+        assertEquals(not, is((Number) 3.0276503540974917));
+        assertEquals(num, not);
     }
 
     /**
-     * Este test comprueba que el método no nos devuelve conjunto vacíos de
+     * Test que comprueba que el método no nos devuelve conjunto vacíos de
      * datos.
-     * 
-     * @throws FilloException
      */
     @Test
-    public void testResultSet() throws FilloException {
-    	String tableName = "N3_Historico";
+    public void testResultSet() {
+    	assertEquals(sistInfData.getResultSet("Prueba", "Numero"), notNullValue());
 
-    	assertEquals(sistInfData.getResultSet(tableName, "Nota",
-                "Descripcion='La descripción del TFG/TFM'"), notNullValue());
+        assertEquals(sistInfData.getResultSet("Prueba", "Nota",
+                "Descripcion='hola'"), notNullValue());
 
-        String[] metricValoreSelect = { "Titulo", "Nota", "Descripcion" };
-        assertEquals(sistInfData.getResultSet(tableName, "Nota", null,
+        String[] metricValoreSelect = { "Numero", "Nota", "Descripcion" };
+        assertEquals(sistInfData.getResultSet("Prueba", "Numero", null,
                 metricValoreSelect), notNullValue());
-        assertEquals(sistInfData.getResultSet(tableName, "Nota",
+        assertEquals(sistInfData.getResultSet("Prueba", "Numero",
                 metricValoreSelect, null), notNullValue());
     }
 
@@ -152,29 +159,31 @@ public class SistInfDataTestXLS {
      * existe pues nos devolverá 0. Y una columna que no existe, por lo que
      * lanzará una excepción .
      * 
-     * @throws FilloException
+     * @throws SQLException
      */
-    public void testTotalNumber() {
-    	String tableName = "N3_Historico";
-        Number obtenido = sistInfData.getTotalNumber("Nota", tableName,
-                "Nota='5'");
-        assertEquals(obtenido, is((Number) 1F));
+    @Test(expected = SQLException.class)
+    public void testTotalNumber() throws SQLException { 
+
+        Number obtenido = sistInfData.getTotalNumber("Nota", "Prueba", "Nota='5'");
+        assertEquals(obtenido, is((Number) 1.0F));
 
         String[] columnNames = { "Nota", "Descripcion" };
-        Number obt = sistInfData.getTotalNumber(columnNames, tableName);
-        assertEquals(obt, is((Number) 8F));
+        Number obt = sistInfData.getTotalNumber(columnNames, "Prueba");
+        assertEquals(obt, is((Number) 12.0F));
 
         columnNames = null;
-        obt = sistInfData.getTotalNumber(columnNames, tableName);
+        obt = sistInfData.getTotalNumber(columnNames, "Prueba");
         assertEquals(obt, is((Number) 0));
+
+       /* String[] columnNam = { "Nada" };
+        obt = sistInfData.getTotalNumber(columnNam, "Prueba");*/
 
     }
 
     @Test
-    public void testQuartilColumn() throws FilloException {
-    	String tableName = "N3_Historico";
+    public void testQuartilColumn() throws SQLException {
     	Double quartil = 0.25;
-    	assertEquals(sistInfData.getQuartilColumn("Nota", tableName, quartil), notNullValue());
+    	assertEquals(sistInfData.getQuartilColumn("Nota", "Prueba", quartil), notNullValue());
     }
 
     /**
@@ -182,40 +191,29 @@ public class SistInfDataTestXLS {
      * mínima funcione correctamente. La fecha mínima que esperamos es
      * 1/01/1649. Y la fecha máxima es 1/01/2015.
      * 
-     * @throws FilloException
+     * @throws SQLException
      */
     @Test
-    public void testFechas() throws FilloException {
-    	String tableName = "N3_Historico";
-        LocalDate esperado = sistInfData.getYear("FechaAsignacion", tableName, true);
-        LocalDate valor = LocalDate.of(2014, 11, 07);
+    public void testFechas() throws SQLException {
+        LocalDate esperado = sistInfData.getYear("Fecha", "Prueba", true);
+        LocalDate valor = LocalDate.of(1649, 1, 1);
 
         assertEqualDates(esperado, valor);
 
-        LocalDate esperado2 = sistInfData.getYear("FechaAsignacion", tableName, false);
-        LocalDate valor2 = LocalDate.of(2017, 10, 06);
-        
+        LocalDate esperado2 = sistInfData.getYear("Fecha", "Prueba", false);
+        LocalDate valor2 = LocalDate.of(2015, 1, 1);
+
         assertEqualDates(esperado2, valor2);
-        
-        LocalDate esperado3 = sistInfData.getYear("FechaPresentacion", tableName, true);
-        LocalDate valor3 = LocalDate.of(2015, 07, 07);
-
-        assertEqualDates(esperado3, valor3);
-
-        LocalDate esperado4 = sistInfData.getYear("FechaPresentacion", tableName, false);
-        LocalDate valor4 = LocalDate.of(2018, 07, 06);
-
-        assertEqualDates(esperado4, valor4);
     }
     
     /**
      * Se comprueba que el formato de las fechas sea dd/mm/yyyy
      * 
-     * @throws FilloException
+     * @throws SQLException
      * @throws ParseException 
      */
     @Test
-    public void testFormatDateHistorico() throws FilloException, ParseException { 
+    public void testFormatDatesHistorico() throws SQLException, ParseException { 
     	String tableName = "N3_Historico";
         List<String> dates = sistInfData.getDates("FechaAsignacion", tableName);
         
@@ -247,24 +245,24 @@ public class SistInfDataTestXLS {
     }
 
     /**
-     * Test que comprueba que salta la excepción FilloException al intentar
+     * Test que comprueba que salta la excepción SQLException al intentar
      * obtener datos de una tabla que no existe.
      * 
-     * @throws FilloException
+     * @throws SQLException
      */
-    @Test(expected = FilloException.class)
-    public void testSinTabla() throws FilloException {
+    @Test(expected = SQLException.class)
+    public void testSinTabla() throws SQLException { 
         sistInfData.getAvgColumn("Nada", "Nada");
     }
 
     /**
-     * Test que comprueba que salta la excepción FilloException al intentar
+     * Test que comprueba que salta la excepción SQLException al intentar
      * obtener datos de una tabla que está vacía.
      * 
-     * @throws FilloException
+     * @throws SQLException
      */
-    @Test(expected = FilloException.class)
-    public void testTablaVacia() throws FilloException {
+    @Test(expected = SQLException.class)
+    public void testTablaVacia() throws SQLException { 
         sistInfData.getAvgColumn("Nada", "Vacia");
     }
 
@@ -274,6 +272,6 @@ public class SistInfDataTestXLS {
      * @param args
      */
     public static void main(String[] args) {
-        JUnitCore.main("ubu.digit.pesistence.SistInfDataTestXLS");
-    }
+        JUnitCore.main("ubu.digit.pesistence.SistInfDataTestCSV");
+    } 
 }
--- src/test/java/ubu/digit/persistence/SistInfDataTestXLS.java
+++ src/test/java/ubu/digit/persistence/SistInfDataTestXLS.java
@@ -1,13 +1,11 @@
-package ubu.digit.pesistence;
+package ubu.digit.persistence;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 
-import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -22,31 +20,31 @@ import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import com.codoid.products.exception.FilloException;
+
+import ubu.digit.persistence.SistInfDataXls;
 import ubu.digit.util.ExternalProperties;
-import ubu.digit.persistence.SistInfDataCsv;
 
 /**
- * Conjunto de método que verifican la cobertura de la clase SistInfDataCsv.
+ * Conjunto de método que verifican la cobertura de la clase SistInfDataXls.
  * 
- * @author Beatriz Zurera Martínez-Acitores
  * @author Diana Bringas Ochoa
  *
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ ExternalProperties.class, SistInfDataCsv.class })
-public class SistInfDataTestCSV {
+@PrepareForTest({ ExternalProperties.class, SistInfDataXls.class })
+public class SistInfDataTestXLS {
 
     /**
      * Clase fachada a testear.
      */
-    SistInfDataCsv sistInfData;
+	SistInfDataXls sistInfData;
 
     /**
      * URL del fichero donde se encuentra el fichero de configuración del test.
      */
-    ExternalProperties test = ExternalProperties
-    		.getInstance("/testConfig.properties", true);
-
+    ExternalProperties test = ExternalProperties.getInstance("/testConfig.properties", true);
+   
     /**
      * Método que se ejecuta antes de cualquier test. Modifica el fichero de
      * configuración, por el propio del test.
@@ -54,23 +52,25 @@ public class SistInfDataTestCSV {
     @Before
     public void setUp() {
         mockStatic(ExternalProperties.class);
+
         when(ExternalProperties.getInstance("/config.properties", false)).thenReturn(test);
-        sistInfData = SistInfDataCsv.getInstance(); 
+
+        sistInfData = SistInfDataXls.getInstance(); 
     }
 
     /**
      * Test que comprueba que el método que obtiene la media de una lista de
      * números funciona correctamente.
-     * 
+     *
      * En este caso, la columna "notas" es una lista de notas desde el 1 hasta
      * el 10 por lo que el resultado que esperamos es 5.5.
      * 
-     * @throws SQLException
+     * @throws FilloException
      */
     @Test
-    public void testAvg() throws SQLException {
-        Number esperado = sistInfData.getAvgColumn("Nota", "Prueba");
-        assertEquals(esperado, is((Number) 5.5F));
+    public void testAvg(){
+        Number esperado = sistInfData.getAvgColumn("Nota", "N3_Historico");
+        assertEquals(esperado, is((Number) 7.0625F));
     }
 
     /**
@@ -78,17 +78,14 @@ public class SistInfDataTestCSV {
      * listado funciona correctamente. En este caso, la columna "numero" tiene
      * como máximo un 9 y la columna "nota" tiene como máximo un 10.
      * 
-     * @throws SQLException
+     * @throws FilloException
      */
     @Test
-    public void testMaxColumn() throws SQLException {
+    public void testMaxColumn() throws FilloException {
 
-        String tableName = "Prueba";
-        Number num = sistInfData.getMaxColumn("Numero", tableName);
+        String tableName = "N3_Historico";
         Number not = sistInfData.getMaxColumn("Nota", tableName);
-        assertEquals(num, is((Number) 9.0F));
-        assertEquals(not, is((Number) 10.0F));
-        assertNotEquals(num, not);
+        assertEquals(not, is((Number) 9F));
     }
 
     /**
@@ -96,16 +93,13 @@ public class SistInfDataTestCSV {
      * listado funciona correctamente. En este caso, la columna "numero" tiene
      * como mínimo un 0 y la columna "nota" tiene como mínimo un 1.
      * 
-     * @throws SQLException
+     * @throws FilloException
      */
     @Test
-    public void testMinColumn() throws SQLException {
-        String tableName = "Prueba";
-        Number num = sistInfData.getMinColumn("Numero", tableName);
+    public void testMinColumn() throws FilloException {
+        String tableName = "N3_Historico";
         Number not = sistInfData.getMinColumn("Nota", tableName);
-        assertEquals(num, is((Number) 0.0F));
-        assertEquals(not, is((Number) 1.0F));
-        assertNotEquals(num, not);
+        assertEquals(not, is((Number) 4F));
     }
 
     /**
@@ -114,33 +108,32 @@ public class SistInfDataTestCSV {
      * un listado de números de 0 a 9 y la columna "nota" un listado de 1 a 10,
      * por lo que en ambas esperamos la misma desviación.
      * 
-     * @throws SQLException
+     * @throws FilloException
      */
     @Test
-    public void testStdvColumn() throws SQLException { 
-        String tableName = "Prueba";
-        Number num = sistInfData.getStdvColumn("Numero", tableName);
+    public void testStdvColumn() throws FilloException { 
+        String tableName = "N3_Historico";
         Number not = sistInfData.getStdvColumn("Nota", tableName);
-        assertEquals(num, is((Number) 3.0276503540974917));
-        assertEquals(not, is((Number) 3.0276503540974917));
-        assertEquals(num, not);
+        assertEquals(not, is((Number) 1.8980723303996008));
     }
 
     /**
-     * Test que comprueba que el método no nos devuelve conjunto vacíos de
+     * Este test comprueba que el método no nos devuelve conjunto vacíos de
      * datos.
+     * 
+     * @throws FilloException
      */
     @Test
-    public void testResultSet() {
-    	assertEquals(sistInfData.getResultSet("Prueba", "Numero"), notNullValue());
+    public void testResultSet() throws FilloException {
+    	String tableName = "N3_Historico";
 
-        assertEquals(sistInfData.getResultSet("Prueba", "Nota",
-                "Descripcion='hola'"), notNullValue());
+    	assertEquals(sistInfData.getResultSet(tableName, "Nota",
+                "Descripcion='La descripción del TFG/TFM'"), notNullValue());
 
-        String[] metricValoreSelect = { "Numero", "Nota", "Descripcion" };
-        assertEquals(sistInfData.getResultSet("Prueba", "Numero", null,
+        String[] metricValoreSelect = { "Titulo", "Nota", "Descripcion" };
+        assertEquals(sistInfData.getResultSet(tableName, "Nota", null,
                 metricValoreSelect), notNullValue());
-        assertEquals(sistInfData.getResultSet("Prueba", "Numero",
+        assertEquals(sistInfData.getResultSet(tableName, "Nota",
                 metricValoreSelect, null), notNullValue());
     }
 
@@ -159,31 +152,29 @@ public class SistInfDataTestCSV {
      * existe pues nos devolverá 0. Y una columna que no existe, por lo que
      * lanzará una excepción .
      * 
-     * @throws SQLException
+     * @throws FilloException
      */
-    @Test(expected = SQLException.class)
-    public void testTotalNumber() throws SQLException { 
-
-        Number obtenido = sistInfData.getTotalNumber("Nota", "Prueba", "Nota='5'");
-        assertEquals(obtenido, is((Number) 1.0F));
+    public void testTotalNumber() {
+    	String tableName = "N3_Historico";
+        Number obtenido = sistInfData.getTotalNumber("Nota", tableName,
+                "Nota='5'");
+        assertEquals(obtenido, is((Number) 1F));
 
         String[] columnNames = { "Nota", "Descripcion" };
-        Number obt = sistInfData.getTotalNumber(columnNames, "Prueba");
-        assertEquals(obt, is((Number) 12.0F));
+        Number obt = sistInfData.getTotalNumber(columnNames, tableName);
+        assertEquals(obt, is((Number) 8F));
 
         columnNames = null;
-        obt = sistInfData.getTotalNumber(columnNames, "Prueba");
+        obt = sistInfData.getTotalNumber(columnNames, tableName);
         assertEquals(obt, is((Number) 0));
-
-       /* String[] columnNam = { "Nada" };
-        obt = sistInfData.getTotalNumber(columnNam, "Prueba");*/
 
     }
 
     @Test
-    public void testQuartilColumn() throws SQLException {
+    public void testQuartilColumn() throws FilloException {
+    	String tableName = "N3_Historico";
     	Double quartil = 0.25;
-    	assertEquals(sistInfData.getQuartilColumn("Nota", "Prueba", quartil), notNullValue());
+    	assertEquals(sistInfData.getQuartilColumn("Nota", tableName, quartil), notNullValue());
     }
 
     /**
@@ -191,29 +182,40 @@ public class SistInfDataTestCSV {
      * mínima funcione correctamente. La fecha mínima que esperamos es
      * 1/01/1649. Y la fecha máxima es 1/01/2015.
      * 
-     * @throws SQLException
+     * @throws FilloException
      */
     @Test
-    public void testFechas() throws SQLException {
-        LocalDate esperado = sistInfData.getYear("Fecha", "Prueba", true);
-        LocalDate valor = LocalDate.of(1649, 1, 1);
+    public void testFechas() throws FilloException {
+    	String tableName = "N3_Historico";
+        LocalDate esperado = sistInfData.getYear("FechaAsignacion", tableName, true);
+        LocalDate valor = LocalDate.of(2014, 11, 07);
 
         assertEqualDates(esperado, valor);
 
-        LocalDate esperado2 = sistInfData.getYear("Fecha", "Prueba", false);
-        LocalDate valor2 = LocalDate.of(2015, 1, 1);
-
+        LocalDate esperado2 = sistInfData.getYear("FechaAsignacion", tableName, false);
+        LocalDate valor2 = LocalDate.of(2017, 10, 06);
+        
         assertEqualDates(esperado2, valor2);
+        
+        LocalDate esperado3 = sistInfData.getYear("FechaPresentacion", tableName, true);
+        LocalDate valor3 = LocalDate.of(2015, 07, 07);
+
+        assertEqualDates(esperado3, valor3);
+
+        LocalDate esperado4 = sistInfData.getYear("FechaPresentacion", tableName, false);
+        LocalDate valor4 = LocalDate.of(2018, 07, 06);
+
+        assertEqualDates(esperado4, valor4);
     }
     
     /**
      * Se comprueba que el formato de las fechas sea dd/mm/yyyy
      * 
-     * @throws SQLException
+     * @throws FilloException
      * @throws ParseException 
      */
     @Test
-    public void testFormatDatesHistorico() throws SQLException, ParseException { 
+    public void testFormatDateHistorico() throws FilloException, ParseException { 
     	String tableName = "N3_Historico";
         List<String> dates = sistInfData.getDates("FechaAsignacion", tableName);
         
@@ -245,24 +247,24 @@ public class SistInfDataTestCSV {
     }
 
     /**
-     * Test que comprueba que salta la excepción SQLException al intentar
+     * Test que comprueba que salta la excepción FilloException al intentar
      * obtener datos de una tabla que no existe.
      * 
-     * @throws SQLException
+     * @throws FilloException
      */
-    @Test(expected = SQLException.class)
-    public void testSinTabla() throws SQLException { 
+    @Test(expected = FilloException.class)
+    public void testSinTabla() throws FilloException {
         sistInfData.getAvgColumn("Nada", "Nada");
     }
 
     /**
-     * Test que comprueba que salta la excepción SQLException al intentar
+     * Test que comprueba que salta la excepción FilloException al intentar
      * obtener datos de una tabla que está vacía.
      * 
-     * @throws SQLException
+     * @throws FilloException
      */
-    @Test(expected = SQLException.class)
-    public void testTablaVacia() throws SQLException { 
+    @Test(expected = FilloException.class)
+    public void testTablaVacia() throws FilloException {
         sistInfData.getAvgColumn("Nada", "Vacia");
     }
 
@@ -272,6 +274,6 @@ public class SistInfDataTestCSV {
      * @param args
      */
     public static void main(String[] args) {
-        JUnitCore.main("ubu.digit.pesistence.SistInfDataTestCSV");
-    } 
+        JUnitCore.main("ubu.digit.pesistence.SistInfDataTestXLS");
+    }
 }
